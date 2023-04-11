66219937496a9c4c12fd1e16e94ee98a7ad73b7c
victorshinkevich <supostatekx@gmail.com>
2023-04-11 04:22:34 +0400

    Add db

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use zero2prod::configuration;
+use zero2prod::startup;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let configuration = configuration::get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    startup::run(listener)?.await
 }
