1786b92aa83838e4a18003f27bff7fd710e3c0aa
victorshinkevich <supostatekx@gmail.com>
2023-04-12 15:53:17 +0400

    APP__ variables and serde aux

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,5 +1,6 @@
 use config::{Config, File};
 use secrecy::{ExposeSecret, Secret};
+use serde_aux::field_attributes::deserialize_number_from_string;
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
@@ -9,6 +10,7 @@ pub struct Settings {
 
 #[derive(serde::Deserialize)]
 pub struct ApplicationSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
 }
@@ -17,6 +19,7 @@ pub struct ApplicationSettings {
 pub struct DatabaseSettings {
     pub username: String,
     pub password: Secret<String>,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
     pub database_name: String,
@@ -82,6 +85,12 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
         .add_source(File::with_name("configuration/base"))
         .add_source(
             File::with_name(&format!("configuration/{}", environment.as_str())).required(false),
+        )
+        .add_source(
+            config::Environment::with_prefix("APP")
+                .try_parsing(true)
+                .separator("__")
+                .list_separator(" "),
         )
         .build()?
         .try_deserialize()
